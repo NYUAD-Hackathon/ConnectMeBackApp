94b5f279f8db4c71f037b2b899e0e7f42e23a95e
joejean <joeclef@hotmail.com>
2015-04-10 22:57:10 +0400

    Firxed bug in run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/", methods= ["GET", "POST"])
 def replyToSms():
 	resp = twilio.twiml.Response()
-	resp.message = ("What is your Name?")
+	resp.message("What is your Name?")
 	return str(resp)
 
 if __name__ == "__main__":
