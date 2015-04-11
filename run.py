490a2660dc443238420ae3aff86dbd1cedc7a8dc
joejean <joeclef@hotmail.com>
2015-04-11 09:30:25 +0400

    Updated the Run.py

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask import request
 import twilio.twiml
 
 app = Flask(__name__)
 
 @app.route("/", methods= ["GET", "POST"])
 def replyToSms():
+	print request.data
 	resp = twilio.twiml.Response()
 	resp.message("What is your Name?")
 	return str(resp), 200, {"Content-Type":"application/xml; charset=utf-8"}
