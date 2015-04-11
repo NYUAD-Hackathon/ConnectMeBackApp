0bd8045c1eaf737c70d106b42792ad60a33e1df0
jocelyn schulz <jocyk@jocelyns-MacBook-Pro.local>
2015-04-11 13:37:17 +0400

    basic config done for parsing and responding to sms

--- run.py
+++ run.py
@@ -1,15 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import re
 from flask import Flask
 from flask import request
-import twilio.twiml
+from twilio import twiml
 
 app = Flask(__name__)
 
 @app.route("/", methods= ["GET", "POST"])
 def replyToSms():
-	messageBody = request.form.get('Body')
-	resp = twilio.twiml.Response()
-	resp.message("What is your Name?")
-	return str(resp), 200, {"Content-Type":"application/xml; charset=utf-8"}
+	resp = twiml.Response()
+	body = request.form.get('Body')
+	tokens = re.sub(r'\s', '', body).split('*')
+	# If the message is malformed, remind the user of format
+	if (len(tokens) != 3):
+		#resp.message("Sorry. Please use format: my name * their name * message")
+		resp.message(u'عذراً. الرجاء إستعمال هذا التصميم : إسمي*اسمهم*الرسالة')
+	else:
+		# TODO : store into DB
+		# TODO : search database and if there's a match, return relevant info. 
+		#resp.message("Bulletin posted. Pending match.")
+		resp.message(u'لقد تم نشر هذا البيان | التطابق  في قيد الانتظار')
+		# if no match, just say bulletin was posted but no match yet.
+	return str(resp)
 
 if __name__ == "__main__":
     app.run(debug=True)
